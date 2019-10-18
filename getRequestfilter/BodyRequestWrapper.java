f5596a8bc55cf7a9879ff507a6c53a9bb0cab50a
AlphaLay <794311913@qq.com>
2019-10-18 19:19:03 +0800

    wrapper

--- getRequestfilter/BodyRequestWrapper.java
+++ getRequestfilter/BodyRequestWrapper.java
@@ -1,28 +1,38 @@
 package com.richinfoai.server.utlis;
 
+import com.google.common.base.Strings;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.servlet.ReadListener;
+import javax.servlet.ServletException;
 import javax.servlet.ServletInputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletRequestWrapper;
+import javax.servlet.http.Part;
 import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Collection;
 
 public class BodyRequestWrapper extends HttpServletRequestWrapper {
 
     //private  String body;
-    private final byte[] binary;
+    //private final byte[] binary;
+    private Collection<Part> parts;
 
 
-    public BodyRequestWrapper(HttpServletRequest request) throws IOException {
+    public BodyRequestWrapper(HttpServletRequest request) throws IOException,ServletException {
         super(request);
-        this.binary = RequestReadUtils.saveRequestBodyBinary(request);
+        //json body
+        //this.binary = RequestReadUtils.saveRequestBodyBinary(request);
+        //RequestPart
+        this.parts = request.getParts();
+        //直接获取字符流也没有问题
         //this.body = RequestReadUtils.saveRequestBodyByInputStream(request);
         //this.body = RequestReadUtils.saveRequestBodyByReader(request);
+        //有文件上传时获取Parts
 
     }
 
@@ -31,9 +41,24 @@ public class BodyRequestWrapper extends HttpServletRequestWrapper {
 //    }
 
     public byte[] getBinary() {
-        return binary;
+        return null;
     }
 
+
+    @Override
+    public Part getPart(String name) throws IOException, ServletException {
+        for (Part part : parts) {
+            if (!Strings.isNullOrEmpty(part.getName()) && part.getName().equals(name))
+                return part;
+        }
+        return null;
+    }
+
+    @Override
+    public Collection<Part> getParts() throws IOException,
+            ServletException {
+        return this.parts;
+    }
 
     @Override
     public ServletInputStream getInputStream() {
@@ -66,7 +91,6 @@ public class BodyRequestWrapper extends HttpServletRequestWrapper {
     public BufferedReader getReader() {
         return new BufferedReader(new InputStreamReader(this.getInputStream()));
     }
-
 
 
 }
--- getRequestfilter/RequestReadUtils.java
+++ getRequestfilter/RequestReadUtils.java
@@ -5,9 +5,17 @@ import org.springframework.lang.Nullable;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.imageio.ImageIO;
+import javax.imageio.ImageReadParam;
+import javax.imageio.ImageReader;
 import javax.servlet.ServletInputStream;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.Part;
+import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.io.*;
+import java.util.Arrays;
+import java.util.Iterator;
 import java.util.Map;
 
 public class RequestReadUtils {
@@ -108,5 +116,67 @@ public class RequestReadUtils {
             }
         }
         return sb.toString();
+    }
+
+    private void commonFileProcess(String filename, InputStream is) {
+        FileOutputStream fos = null;
+        try {
+            fos = new FileOutputStream(new File(getClass().getResource("/").getPath() + filename));
+            int b = 0;
+            while ((b = is.read()) != -1) {
+                fos.write(b);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                fos.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void imageProcess(String filename, String ext, InputStream is) throws IOException {
+        Iterator<ImageReader> irs = ImageIO.getImageReadersByFormatName(ext);
+        ImageReader ir = irs.hasNext() ? irs.next() : null;
+        if (ir == null)
+            return;
+        ir.setInput(ImageIO.createImageInputStream(is));//必须转换为ImageInputStream，否则异常
+
+        ImageReadParam rp = ir.getDefaultReadParam();
+        Rectangle rect = new Rectangle(0, 0, 200, 200);
+        rp.setSourceRegion(rect);
+
+        int imageNum = ir.getNumImages(true);//allowSearch必须为true，否则有些图片格式imageNum为-1。
+
+        System.out.println("imageNum:" + imageNum);
+
+        for (int imageIndex = 0; imageIndex < imageNum; imageIndex++) {
+            BufferedImage bi = ir.read(imageIndex, rp);
+            ImageIO.write(bi, ext, new File(getClass().getResource("/").getPath() + filename));
+        }
+    }
+
+    private void FileProcess(Part part) throws IOException {
+        System.out.println("part.getName(): " + part.getName());
+
+        if (part.getName().equals("file")) {
+            String cd = part.getHeader("Content-Disposition");
+            String[] cds = cd.split(";");
+            String filename = cds[2].substring(cds[2].indexOf("=") + 1).substring(cds[2].lastIndexOf("//") + 1).replace("\"", "");
+            String ext = filename.substring(filename.lastIndexOf(".") + 1);
+
+            System.out.println("filename:" + filename);
+            System.out.println("ext:" + ext);
+
+            InputStream is = part.getInputStream();
+
+            if (Arrays.binarySearch(ImageIO.getReaderFormatNames(), ext) >= 0)
+                imageProcess(filename, ext, is);
+            else {
+                commonFileProcess(filename, is);
+            }
+        }
     }
 }
